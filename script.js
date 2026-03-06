ab3d57b928e144cb2800493c61adeba7f30c53fd
26rdo-ops <26rdo@ggusd.net>
2026-03-06 09:42:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Slideshow Logic
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  if(slides.length > 0) {
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+}
+
+// Lightbox Logic
+function openModal(src) {
+  document.getElementById("img01").src = src;
+  document.getElementById("myModal").style.display = "block";
+}
